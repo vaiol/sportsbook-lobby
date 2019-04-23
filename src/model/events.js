edaa7a4141b05a2b68c8989e341fa5da456dd2f4
Alexander Stepanov <vaiol.ans@gmail.com>
2019-04-23 11:05:24 +0300

    model events updated

--- src/model/events.js
+++ src/model/events.js
@@ -1,29 +1,36 @@
 import data from "@/assets/sport-events.json";
 
+/**
+ * This file should be rewritten when backend will available.
+ * HTTP request should be here!
+ */
 export default {
   list({ sports, tags, minOdds, page = 1, pageSize = 10 } = {}) {
-    // return full list of events
-    let array = data.events;
+    // return full list of events with query and pagination
+    // query:
+    let events = data.events;
     if (sports && Array.isArray(sports) && sports.length) {
-      array = array.filter(event => sports.includes(event.event.sport));
+      events = events.filter(event => sports.includes(event.event.sport));
     }
     if (tags && Array.isArray(tags) && tags.length) {
-      array = array.filter(event =>
+      events = events.filter(event =>
         tags.some(r => event.event.tags.includes(r))
       );
     }
     if (minOdds) {
-      array = array.filter(event =>
+      events = events.filter(event =>
         event.betOffers.some(bet =>
           bet.outcomes.some(odds => 1 + odds.oddsAmerican / 100 >= minOdds)
         )
       );
     }
+    // pagination:
     const skip = (page - 1) * pageSize;
+    events = events.slice(skip, skip + pageSize);
     return {
-      events: array.slice(skip, skip + pageSize),
+      events,
       pagination: {
-        length: Math.ceil(array.length / pageSize)
+        length: Math.ceil(events.length / pageSize)
       }
     };
   },
@@ -35,7 +42,11 @@ export default {
     // return unique list of all tags
     const tags = [];
     const tags2D = data.events.map(event => event.event.tags);
-    for (const row of tags2D) for (const e of row) tags.push(e);
+    for (const row of tags2D) {
+      for (const e of row) {
+        tags.push(e);
+      }
+    }
     return Array.from(new Set(tags));
   }
 };
